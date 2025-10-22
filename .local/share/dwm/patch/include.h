ac6678b5446bf18df8eefa8ba1e3f8c934d4b521
mafuba <tulkidg@gmail.com>
2025-10-22 15:35:58 +0300

    Initial commit

---
+++ .local/share/dwm/patch/include.h
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_winicon.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "decorationhints.h"
+#include "ipc.h"
+#include "ipc/ipc.h"
+#include "ipc/util.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "seamless_restart.h"
+#include "tapresize.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_centeredmaster.h"
+#include "layout_centeredfloatingmaster.h"
+#include "layout_grid.h"
+#include "layout_horizgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
